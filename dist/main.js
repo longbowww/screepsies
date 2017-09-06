31d7c7c1257e22812759ff2d9904a51f61b39575
Bernhard Schrantz <bernhard@itn-solutions.at>
2017-09-06 20:24:34 +0200

    just console

--- dist/main.js
+++ dist/main.js
@@ -1,27 +1,5 @@
 "use strict";
 
 module.exports.loop = function () {
-    var spawns = Game.spawns;
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
-
-    try {
-        for (var _iterator = spawns[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-            var s = _step.value;
-        }
-    } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-    } finally {
-        try {
-            if (!_iteratorNormalCompletion && _iterator.return) {
-                _iterator.return();
-            }
-        } finally {
-            if (_didIteratorError) {
-                throw _iteratorError;
-            }
-        }
-    }
+    console.log(Game.spawns);
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
 module.exports.loop = function () {
-    let spawns = Game.spawns;
-    for (let s of spawns) {
-    
-    }
+    console.log(Game.spawns);
 };
